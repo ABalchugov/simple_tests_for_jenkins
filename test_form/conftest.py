3a1317128af920dd952f88fbb2dc62c6c1464de2
ABalchugov <inspektr1994@gmail.com>
2026-07-10 16:35:04 +0300

    feature: add attachments

--- test_form/conftest.py
+++ test_form/conftest.py
@@ -1,5 +1,7 @@
 import pytest
 from selenium import webdriver
+from utils import attach
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -10,12 +12,32 @@ def session_lifecycle():
 
 
 @pytest.fixture(scope="function", autouse=True)
-def driver():
+def driver(request):
     print("\n\n>>> Открываем браузер <<<")
-    driver = webdriver.Chrome()
-    driver.maximize_window()
+    # driver = webdriver.Chrome()
+    # driver.maximize_window()
+
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "128.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
 
     yield driver
+
+    attach.add_html(driver)
+    attach.add_screenshot(driver)
+    attach.add_logs(driver)
+    attach.add_video(driver)
 
     print("\n\n>>> Закрываем браузер <<<")
     driver.quit()
