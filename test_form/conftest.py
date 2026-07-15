41d95566805afb15b22912cdc21b19319d6bc433
ABalchugov <inspektr1994@gmail.com>
2026-07-15 21:06:08 +0300

    feature: add options

--- test_form/conftest.py
+++ test_form/conftest.py
@@ -1,7 +1,8 @@
 import pytest
 from selenium import webdriver
 from utils import attach
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from dotenv import load_dotenv
 import os
 
@@ -9,12 +10,59 @@ import os
 def load_env():
     load_dotenv()
 
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser",
+        default="chrome",
+        choices=("chrome", "firefox"),
+        help="Browser to use"
+    )
+    parser.addoption(
+        "--browser_version",
+        default="150.0",
+        choices=("152.0", "151.0", "150.0", "149.0", "148.0"),
+        help="Browser version to use"
+    )
+    parser.addoption(
+        "--headless",
+        default=False,
+        choices=(True, False),
+        help="Browser version to use"
+    )
+    parser.addoption(
+        "--window_size",
+        default="1920*1080",
+        help="Window size"
+    )
+
+
 @pytest.fixture(scope="function", autouse=True)
 def driver(request):
-    options = Options()
+    browser = request.config.getoption("--browser")
+    browser_version = request.config.getoption("--browser_version")
+    headless = request.config.getoption("--headless")
+    window_size = request.config.getoption("--window_size")
+    width_str, height_str = window_size.split("*", 1)
+
+    width = int(width_str) if width_str else 1920
+    height = int(height_str) if height_str else 1080
+
+    options = None
+
+    if browser == "chrome":
+        options = ChromeOptions()
+        options.add_argument(f"--window-size={width},{height}")
+    elif browser == "firefox":
+        options = FirefoxOptions()
+        options.add_argument(f"--width={width}")
+        options.add_argument(f"--height={height}")
+
+    if headless:
+        options.add_argument("--headless")
+
     selenoid_capabilities = {
-        "browserName": "chrome",
-        "browserVersion": "148.0",
+        "browserName": browser,
+        "browserVersion": browser_version,
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
