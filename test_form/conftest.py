a47c065f73a7fe7fb04a4de77f9bd60a80a836b1
ABalchugov <inspektr1994@gmail.com>
2026-07-15 19:56:28 +0300

    feature add .env

--- test_form/conftest.py
+++ test_form/conftest.py
@@ -2,7 +2,12 @@ import pytest
 from selenium import webdriver
 from utils import attach
 from selenium.webdriver.chrome.options import Options
+from dotenv import load_dotenv
+import os
 
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    load_dotenv()
 
 @pytest.fixture(scope="function", autouse=True)
 def driver(request):
@@ -16,8 +21,10 @@ def driver(request):
         }
     }
     options.capabilities.update(selenoid_capabilities)
+    selenoid_login = os.getenv("SELENOID_LOGIN")
+    selenoid_pass = os.getenv("SELENOID_PASS")
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{selenoid_login}:{selenoid_pass}@selenoid.autotests.cloud/wd/hub",
         options=options
     )
 
